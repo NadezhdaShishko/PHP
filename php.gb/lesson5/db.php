2d0455f3f141094ed3d9b09b36da210edb4e8134
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-15 02:38:57 +0300

    отредактирован

--- php.gb/lesson5/db.php
+++ php.gb/lesson5/db.php
@@ -1,14 +1,11 @@
 <?php
 /* Подключение к БД */ 
-$mysqli = mysqli_connect(HOST, USER, PASSWORD, DB) or die ("Невозможно подключиться к БД");
-mysqli_query($mysqli, "SET CHARACTER SET 'utf8'");
-
-$query = mysqli_query($mysqli, "SELECT * FROM gallery.images ORDER BY views DESC");
-
-$result = [];
-
-while ($row = mysqli_fetch_assoc($query)) {
-    $result[] = $row;
-}
-
-mysqli_close($mysqli);
+$localConfig = include 'config.php';
+$config = array_merge($localConfig);
+$mysqli = mysqli_connect(
+    $config['db_host'],
+    $config['db_user'],
+    $config['db_pass'],
+    $config['db_name']
+) 
+or die ("Невозможно подключиться к БД");
