042a2740de207302ffaeaa03eada980e0ab6aa84
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-15 02:37:37 +0300

    исправлен

--- php.gb/lesson5/viewsImage.php
+++ php.gb/lesson5/viewsImage.php
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Views of photo</title>
-</head>
-<body>
+<?php
+include 'db.php';
 
-<?php foreach ($result as $image): ?>
-    <img src="<?= $image['path'] . $image['name'] ?>">
-    <p> Просмотров: <?= $image['views'] ?> </p>
-<?php endforeach; ?>
+$tpl = file_get_contents('viewsImage.tpl');
 
-</body>
-</html>
+$id = (int)($_GET['id']);
+$query = mysqli_query($mysqli, "SELECT * FROM images WHERE id = '$id'");
+$sql = mysqli_query($mysqli, "UPDATE images SET views = views + 1 WHERE id = '$id'");
+
+if(mysqli_num_rows($query) !==0){
+    mysqli_query($mysqli, $sql);
+    $row = mysqli_fetch_assoc($query);
+    $content = '<img src="'.$row['path'] . $row['name'].'" alt="'.$row['$name'].'">';
+    $content .= '<br> Просмотров: ' .($row['views']+1);
+    $title .= 'image id = ' .$id;
+}
+
+mysqli_close($mysqli);
+
+$patterns = array('/{title}/', '/{content}/');
+$replace = array($title, $content);
+echo preg_replace($patterns, $replace, $tpl);
