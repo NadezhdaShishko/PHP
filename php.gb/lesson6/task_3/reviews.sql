838a2909d5c6eea805f0f75daf838dd1d48c9b8f
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-16 15:07:50 +0300

    бд

---
+++ php.gb/lesson6/task_3/reviews.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Май 16 2019 г., 14:56
+-- Версия сервера: 5.6.43
+-- Версия PHP: 7.1.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `reviews`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(10) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `otziv` text NOT NULL,
+  `page` varchar(128) NOT NULL,
+  `date` varchar(64) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `name`, `email`, `otziv`, `page`, `date`) VALUES
+(11, 'Alex', 'eastcon64@mail.ru', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quidem sequi debitis!', '', '16.05.2019 - 14:52'),
+(1, 'Иван', 'julapo@mailapps.online', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quidem sequi debitis!', '', '16.05.2019 - 14:40'),
+(2, 'krosh', ' ditona ditona@mail.ru', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quidem sequi debitis!', '', '16.05.2019 - 14:43');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
