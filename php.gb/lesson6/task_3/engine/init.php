9e33a0205c7afcf383f3e192edb125814578d2d3
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-16 15:04:54 +0300

    init

---
+++ php.gb/lesson6/task_3/engine/init.php
@@ -0,0 +1,26 @@
+<?php
+
+define('ROOT_DIR', __DIR__.'/../');
+
+$defaultConfig = require ROOT_DIR.'config/config.default.php';
+
+
+if (!file_exists(ROOT_DIR.'config/config.php')) {
+    echo 'Создайте файл конфигурации';
+    $localConfig = [];
+}
+
+else {
+    $localConfig = require ROOT_DIR.'config/config.php';
+}
+
+$config = array_merge($defaultConfig, $localConfig);
+
+
+$mysqli = mysqli_connect(
+    $config['db_host'],
+    $config['db_user'],
+    $config['db_pass'],
+    $config['db_name']
+
+);
