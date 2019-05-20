3ba941e5abb67c84779da7e14f7c03175d99aefe
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-20 16:00:55 +0300

    templates

---
+++ php.gb/MyProjectHTMLProf(PHP)/templates/navNewArrivals.php
@@ -0,0 +1,12 @@
+<div class="newArrivalsWrap">
+    <div class="container newArrivals">
+        <h1>New arrivals</h1>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="../public/index.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="../public/product.php">Men</a></li>
+                <li class="breadcrumb-item active" aria-current="../public/product.php">New arrivals</a></li>
+            </ol>
+        </nav>
+    </div>
+</div>
