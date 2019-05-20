5ef8e267516feb349ec5e4d23a1cc44fea3dba94
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-20 16:00:29 +0300

    templates

---
+++ php.gb/MyProjectHTMLProf(PHP)/templates/topNav.php
@@ -0,0 +1,25 @@
+<nav class="topNav">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="../public/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Man</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Women</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Kids</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Accoseriese</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Featured</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hot Deals</a>
+        </li>
+    </ul>
+</nav>
