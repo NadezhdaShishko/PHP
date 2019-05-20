69d5cdbe6ceae7a7a8e95af4e12c64d3ad8fa12a
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-20 15:40:55 +0300

    немного подправила простой калькулятор

--- php.gb/lesson6/calc_1.php
+++ php.gb/lesson6/calc_1.php
@@ -8,10 +8,10 @@ function clearData($data, $type='i') {
 
 $result = "";
 
-if($_SERVER['REQUEST_METHOD'] == "POST") {
-    $num1 = clearData($_POST['num1']);
-    $num2 = clearData($_POST['num2']);
-    $operation = clearData( $_POST['operation'], 's');
+if(($_GET['num1'] != '') && ($_GET['operation']!= '') && ($_GET['num2'] != '')) {
+    $num1 = clearData($_GET['num1']);
+    $num2 = clearData($_GET['num2']);
+    $operation = clearData($_GET['operation'], 's');
     $result = "$num1 $operation $num2 = ";
 
     switch ($operation) {
@@ -33,6 +33,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
         default:
             $result = 'Такой арифметической операции нет!';
     }
+} else { 
+    $result = "Введите число!";
 }
 ?>
 
@@ -40,30 +42,32 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    
     <title> Calculator </title>
+    <link rel="stylesheet" href='css/style.css'/>
 </head>
 
 <body>
     <div class="container">
         <h2>Калькулятор</h2>
-        <form class="calc1" action="<? $_SERVER['PHP_SELF'] ?>" method='POST'>
-            <input type="text" name="num1" placeholder="Введите первое число">
+        <form class="calc1" method='GET'>
+            <input type="text" name="num1" value="<?=$num1?>" placeholder="Введите первое число">
             <select name="operation">
-                <option value="+">+</option>
-                <option value="-">-</option>
-                <option value="*">*</option>
-                <option value="/">/</option>
+                <option <?php if ($operation == "+") echo "selected"?> value="+">+</option>
+                <option <?php if ($operation == "-") echo "selected"?> value="-">-</option>
+                <option <?php if ($operation == "*") echo "selected"?> value="*">*</option>
+                <option <?php if ($operation == "/") echo "selected"?> value="/">/</option>
             </select>
-            <input type="text" name="num2" placeholder="Введите второе число">
+            <input type="text" name="num2" value="<?=$num2?>" placeholder="Введите второе число">
             <input type="submit" value="Посчитать">
         </form>
-
-        <?php
-        if($result){
-            echo '<h2>'.$result.'</h2>';
-        }
-        ?>
+        <div class="result">
+            <?php
+            if($result){
+                echo '<h2>'.$result.'</h2>';
+            }
+            ?>
+        </div>
     </div>
     
 </body>
