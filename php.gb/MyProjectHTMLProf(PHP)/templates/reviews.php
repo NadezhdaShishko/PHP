7e5d13af9ffc1219959331e766b94b65bde936d8
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-20 16:00:38 +0300

    templates

---
+++ php.gb/MyProjectHTMLProf(PHP)/templates/reviews.php
@@ -0,0 +1,38 @@
+<div class="container-fluid subscribeWrap">
+    <div class="container subscribe">
+        <div class="row reviews">
+            <div class="col-md-6 coll-sm-12 reviewUnit">
+                <img src="./images/reviewPerson.png" alt="">
+                <div class="reviewUnitRight">
+                    <blockquote>
+                        “Vestibulum quis porttitor dui! Quisque viverra nunc mi,
+                        a pulvinar purus condimentum a. Aliquam condimentum mattis neque sed pretium”
+                    </blockquote>
+                    <div class="reviewName">
+                        Bin Burhan
+                    </div>
+                    <div class="reviewFrom">
+                        Dhaka, Bd
+                    </div>
+                    <div class="reviewsSwitch">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 coll-sm-12 subscribeRight">
+                <div class="subscribeRightHead">
+                    Subscribe
+                </div>
+                <div class="subscribeRightText">
+                    FOR OUR NEWLETTER AND PROMOTION
+                </div>
+                <form class="subscribeForm" action="">
+                    <input placeholder="Enter Your Email" type="text">
+                    <button>Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
