f7bc715e472b18ab1f199bdb4c12803b172a2b80
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-20 15:57:45 +0300

    index

---
+++ php.gb/MyProjectHTMLProf(PHP)/public/index.php
@@ -0,0 +1,12 @@
+<?php
+require './../engine/init.php';
+$title = 'Brand-Main';
+require '../public/main-goods.php';
+require ROOT_DIR.'/templates/header.php';
+require ROOT_DIR.'/templates/topNav.php';
+require ROOT_DIR.'/templates/banners.php';
+require ROOT_DIR.'templates/main-goods.php';
+require ROOT_DIR.'/templates/offer.php';
+require ROOT_DIR.'/templates/reviews.php';
+require ROOT_DIR.'/templates/footer.php';
+?>
