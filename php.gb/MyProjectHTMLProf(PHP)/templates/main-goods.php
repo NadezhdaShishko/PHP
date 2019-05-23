c680b320a51e766c026a4a29f5cc2e5102d66484
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-23 19:39:48 +0300

    подключила функцию

--- php.gb/MyProjectHTMLProf(PHP)/templates/main-goods.php
+++ php.gb/MyProjectHTMLProf(PHP)/templates/main-goods.php
@@ -3,25 +3,29 @@
     <div class="bottomFetured">Shop for items based on what we featured in this week</div>
     
     <div class="row productUnitContent">
-    <?php foreach ($result as $product):?>
-        <div class="col-lg-3 col-md-4 col-sm-6 productUnit">
-            <div class="productUnitImgWrap">
-                <div class="productUnitBuy">
-                    <button>
-                        <img src="images/addToCart.png" alt="">
-                        Add to Cart
-                    </button>
+    <?php
+    $result = allGoods($mysqli);
+    if($result){
+        foreach ($result as $product):?>
+            <div class="col-lg-3 col-md-4 col-sm-6 productUnit">
+                <div class="productUnitImgWrap">
+                    <div class="productUnitBuy">
+                        <button>
+                            <img src="images/addToCart.png" alt="">
+                            Add to Cart
+                        </button>
+                    </div>
+                    <img src="./<?= $product['img'] ?>" alt="<?= $product['img'] ?>">
                 </div>
-                <img src="./<?= $product['img'] ?>" alt="<?= $product['img'] ?>">
+                <div class="productName">
+                    <a href="../public/singlePage.php?id=<?= $product['id'] ?>" title="Нажмите для просмотра этого товара"> <?= $product['brand'].$product['name'] ?> </a>
+                </div>
+                <div class="productPrice">
+                    $<?= $product['price'] ?>.00
+                </div>
             </div>
-            <div class="productName">
-                <a href="sinlePage.php?id=<?= $product['id'] ?>" title="Нажмите для просмотра этого товара"> <?= $product['name'] ?> </a>
-            </div>
-            <div class="productPrice">
-                $<?= $product['price'] ?>.00
-            </div>
-        </div>
         <?php endforeach ?>
+    <?php } ?>
     </div>
     
     <button class="productBtn">
