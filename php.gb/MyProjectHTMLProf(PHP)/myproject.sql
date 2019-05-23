3cd13914257d7856f1859b832fdcdbadb4ad78d8
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-23 19:30:24 +0300

    bd

--- php.gb/MyProjectHTMLProf(PHP)/myproject.sql
+++ php.gb/MyProjectHTMLProf(PHP)/myproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 20 2019 г., 18:49
+-- Время создания: Май 23 2019 г., 18:54
 -- Версия сервера: 5.6.43
 -- Версия PHP: 7.1.22
 
@@ -25,34 +25,99 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `basket`
+--
+
+CREATE TABLE `basket` (
+  `id` int(12) NOT NULL,
+  `id_item` int(12) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `price` int(12) NOT NULL,
+  `count` int(12) NOT NULL,
+  `login` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(12) NOT NULL,
+  `login` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `date` datetime NOT NULL,
+  `admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `goods`
 --
 
 CREATE TABLE `goods` (
   `id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `img` varchar(128) NOT NULL,
+  `category` varchar(64) NOT NULL,
+  `brand` varchar(64) NOT NULL,
+  `desinger` varchar(64) NOT NULL,
+  `material` varchar(64) NOT NULL,
+  `color` varchar(64) NOT NULL,
+  `size` varchar(64) NOT NULL,
+  `description` text NOT NULL,
   `price` int(12) NOT NULL,
-  `category` varchar(64) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `img` varchar(128) NOT NULL,
+  `is_active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `goods`
 --
 
-INSERT INTO `goods` (`id`, `name`, `img`, `price`, `category`) VALUES
-(1, 'Mango  People  T-shirt', 'images/product_1.jpg', 52, 'featured'),
-(2, 'Mango  People  T-shirt', 'images/product_2.jpg', 52, 'featured'),
-(3, 'Mango  People  T-shirt', 'images/product_3.jpg', 52, 'featured'),
-(4, 'Mango  People  T-shirt', 'images/product_4.jpg', 52, 'featured'),
-(5, 'Mango  People  T-shirt', 'images/product_5.jpg', 52, 'featured'),
-(6, 'Mango  People  T-shirt', 'images/product_6.jpg', 52, 'featured'),
-(7, 'Mango  People  T-shirt', 'images/product_7.jpg', 52, 'featured'),
-(8, 'Mango  People  T-shirt', 'images/product_8.jpg', 52, 'featured');
+INSERT INTO `goods` (`id`, `name`, `category`, `brand`, `desinger`, `material`, `color`, `size`, `description`, `price`, `quantity`, `img`, `is_active`) VALUES
+(1, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_1.jpg', 1),
+(2, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_2.jpg', 1),
+(3, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_3.jpg', 1),
+(4, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_4.jpg', 1),
+(5, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_5.jpg', 1),
+(6, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_6.jpg', 1),
+(7, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_7.jpg', 1),
+(8, 'Mango  People  T-shirt', 'featured', '', '', '', '', '', '', 52, 0, 'images/product_8.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(12) NOT NULL,
+  `login` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `date` datetime NOT NULL,
+  `admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `basket`
+--
+ALTER TABLE `basket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `goods`
@@ -61,14 +126,38 @@ ALTER TABLE `goods`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `basket`
+--
+ALTER TABLE `basket`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `goods`
 --
 ALTER TABLE `goods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
