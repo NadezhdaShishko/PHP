44ec5149c3d975bcf857afd63f49f231b9bcf499
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-25 00:13:22 +0300

    admin

--- php.gb/MyProjectHTMLProf(PHP)/engine/db-goods.php
+++ php.gb/MyProjectHTMLProf(PHP)/engine/db-goods.php
@@ -1,9 +1,9 @@
 <?php
 require 'init.php';
 // echo 'ok';
-function addGoods ($mysqli, $name, $category, $brand, $desinger, $material, $size, $description, $price, $quantity, $img) {
+function addGoods ($mysqli, $name, $category, $brand, $designer, $material, $color, $size, $description, $price, $quantity, $img) {
     
-    $SQL_query = sprintf("INSERT INTO goods (name, category, brand, desinger, material, size, description, price, quantity, img) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s', '%s')", $name, $category, $brand, $desinger, $material, $size, $description, $price, $quantity, $img);
+    $SQL_query = sprintf("INSERT INTO goods (name_item, category, brand, designer, material, size, color, desc_item, price, quantity, img) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s','%s', '%s') ", $name, $category, $brand, $designer, $material, $color, $size, $description, $price, $quantity, $img);
     
     session_start();
     
@@ -21,6 +21,25 @@ function addGoods ($mysqli, $name, $category, $brand, $desinger, $material, $siz
 
 function allGoods ($mysqli) {
     $SQL_query = "SELECT * FROM myproject.goods ORDER BY id DESC";
+    
+    session_start();
+    
+    $query = mysqli_query(myDB_connect(), $SQL_query);
+
+    $result = [];
+
+    while ($row = mysqli_fetch_assoc($query)) {
+        $result[] = $row;
+    }
+
+    return $result;
+    
+    $buffer = ob_get_clean();
+    echo $buffer;
+}
+
+function mainGoods ($mysqli) {
+    $SQL_query = "SELECT * FROM myproject.goods LIMIT 8";
     
     session_start();
     
@@ -56,31 +75,36 @@ function getGoods($mysqli, $id) {
     echo $buffer;
 }
 
-function deleteGooods($mysqli, $id) {
+function deleteGoods($mysqli, $id) {
     $id = (int)$id;
 
     if($id == 0) 
         return false;
 
-    $SQL_query = sprintf("SELECT * FROM myproject.goods WHERE id='%d'", (int)$id);
+    $SQL_query = sprintf("DELETE FROM myproject.goods WHERE id='%d'", (int)$id);
+    session_start();
     $query = mysqli_query(myDB_connect(), $SQL_query);
 
     if(!$query)
-        die (mysqli_error($mysqli));
+        die (mysqli_error(myDB_connect()));
 
-    return mysqli_affected_rows($mysqli);
+    return mysqli_affected_rows(myDB_connect());
 }
 
-function editGooods($mysqli, $id, $name, $category, $brand, $desinger, $material, $color, $size, $description, $price, $quantity, $img) {
+
+function editGoods($mysqli, $id, $name, $category, $brand, $designer, $material, $color, $size, $description, $price, $quantity, $img) {
     $id = (int)$id;
     
-    $sql = "UPDATE goods SET (name='%s', category='%s', brand='%s', desinger='%s', material='%s', color='%s', size='%s', description='%s', price='%s', quantity='%s', img='%s') WHERE id='%d'";
-    $SQL_query = sprintf($sql, mysqli_real_escape_string($mysqli, $name), mysqli_real_escape_string($mysqli, $category), mysqli_real_escape_string($mysqli, $brand), mysqli_real_escape_string($mysqli, $desinger), mysqli_real_escape_string($mysqli, $material), mysqli_real_escape_string($mysqli, $color), mysqli_real_escape_string($mysqli, $size), mysqli_real_escape_string($mysqli, $description), mysqli_real_escape_string($mysqli, $price), mysqli_real_escape_string($mysqli, $quantity), mysqli_real_escape_string($mysqli, $img), mysqli_real_escape_string($mysqli, $id));
+    $sql = "UPDATE myproject.goods SET name_item='%s', category='%s', brand='%s', designer='%s', material='%s', color='%s', size='%s', desc_item='%s', price='%s', quantity='%s', img='%s' WHERE id='%d'";
+    $SQL_query = sprintf($sql, $name, $category, $brand, $designer, $material, $color, $size, $description, $price, $quantity, $img, (int)$id);
     session_start();
-    $query = mysqli_query($mysqli, $SQL_query);
+
+    $query = mysqli_query(myDB_connect(), $SQL_query);
 
     if(!$query)
-        die (mysqli_error($mysqli));
+        die (mysqli_error(myDB_connect()));
 
-    return mysqli_affected_rows($mysqli);
+    return mysqli_affected_rows(myDB_connect());
+    $buffer = ob_get_clean();
+    echo $buffer;
 }
