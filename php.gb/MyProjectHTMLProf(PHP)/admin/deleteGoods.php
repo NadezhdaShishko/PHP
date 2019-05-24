bad6b2ded4f5b48a146f1b97d99f31127614eb4c
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-05-25 00:12:52 +0300

    admin

--- php.gb/MyProjectHTMLProf(PHP)/admin/deleteGoods.php
+++ php.gb/MyProjectHTMLProf(PHP)/admin/deleteGoods.php
@@ -2,8 +2,8 @@
 require './../engine/db-goods.php';
 if($_GET['id']) {
     $id = (int)($_GET['id']);
-}
-$item = deleteGoods($mysqli, $id);
-header: ("Location: ../admin/index.php");
+
+deleteGoods($mysqli, $id);
+header ("Location: ../admin/index.php");}
 $title = 'Admin-DeleteGoods';
 ?>
